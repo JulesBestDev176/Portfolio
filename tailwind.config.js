c9790163a607b3a12b505b5fb30b77a5f158b3c1
JulesBestDev176 <souleymanefall176@gmail.com>
2024-09-09 22:30:44 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: 'Oswald, ui-serif', // Adds a new `font-display` class
+      }
+    }
+  },
+  plugins: [],
+}
+
