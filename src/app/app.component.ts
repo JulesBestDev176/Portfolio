709c31ba103d7cecc86fd2f0dc686eca2946cb1a
JulesBestDev176 <souleymanefall176@gmail.com>
2024-09-10 00:43:41 +0000

    accueil gauche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./composants/navbar/navbar.component";
+import {AccueilComponent} from "./composants/accueil/accueil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
